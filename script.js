12225235d8eca383fcf064f6d6ff1de6bff67031
Ozkar Alvarez <ozkaralvarez98@hotmail.com>
2022-02-27 18:13:29 -0800

    Added layout and button to clear grid

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 let gridContainer = document.querySelector(".grid-container");
 let gridRow;
 let gridItem;
+let clearGridBtn = document.querySelector(".clear-grid");
+let dimension;
 
-for (let i = 1; i <= 16; i++) {
+clearGridBtn.addEventListener("click", () => {
+  window.location.reload();
+});
+
+do {
+  dimension = parseInt(
+    prompt("Enter number of squares per side. Max number = 100.")
+  );
+} while (dimension < 0 || dimension > 100 || isNaN(dimension));
+
+for (let i = 1; i <= dimension; i++) {
   gridRow = document.createElement("div");
   gridRow.className = "grid-row";
   gridContainer.appendChild(gridRow);
-  for (let x = 1; x <= 16; x++) {
+  for (let x = 1; x <= dimension; x++) {
     gridItem = document.createElement("div");
     gridItem.className = "grid-item hovered";
     gridRow.appendChild(gridItem);
