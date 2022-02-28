dea4c9a2bac6051587359e5ad2ea1bfd7587c94a
Ozkar Alvarez <ozkaralvarez98@hotmail.com>
2022-02-28 10:29:19 -0800

    on hover, style every grid item to random rgb

--- script.js
+++ script.js
@@ -4,20 +4,30 @@ let gridRow;
 let gridItem;
 let dimension;
 
-// On click event, remove 'hovered' class
-clearGridBtn.addEventListener("click", () => {
-  let item = document.querySelectorAll(".hovered");
-  item.forEach((e) => {
-    e.classList.remove("hovered");
-  });
-});
-
 // prompt user for grid dimension
 do {
   dimension = parseInt(
     prompt("Enter number of squares per side. Max number = 100.")
   );
 } while (dimension < 0 || dimension > 100 || isNaN(dimension));
+
+// generate random rbg value
+function random_rgba() {
+  var o = Math.round,
+    r = Math.random,
+    s = 255;
+  return (
+    "rgba(" +
+    o(r() * s) +
+    "," +
+    o(r() * s) +
+    "," +
+    o(r() * s) +
+    "," +
+    r().toFixed(1) +
+    ")"
+  );
+}
 
 // create grid and append grid items
 for (let i = 1; i <= dimension; i++) {
@@ -28,8 +38,17 @@ for (let i = 1; i <= dimension; i++) {
     gridItem = document.createElement("div");
     gridItem.className = "grid-item";
     gridRow.appendChild(gridItem);
+    // on hover event, style grid-item to random rbg value
     gridItem.addEventListener("mouseover", (e) => {
-      e.path[0].classList.toggle("hovered");
+      e.path[0].style.backgroundColor = random_rgba();
     });
   }
 }
+
+// On click of button, style grid items back to aqua.
+clearGridBtn.addEventListener("click", () => {
+  let item = document.querySelectorAll(".grid-item");
+  item.forEach((e) => {
+    e.style.backgroundColor = "aqua";
+  });
+});
